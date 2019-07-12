e9f7b36cea123e5977240a0dcdab25fe4563e99f
Etta Rapp <ettirapp@gmail.com>
2019-07-12 15:21:22 -0400

    summarized and tidied census data

--- raw_data/loading_2010_block_level_race_census_data.R
+++ raw_data/loading_2010_block_level_race_census_data.R
@@ -7,14 +7,14 @@ readRenviron("~/.Renviron")
 
 vars = c("P003001", "P003002", "P003003", "P003004", "P003005", "P003006", "P003007", "P003008")
 counties = c("Richmond", "Kings", "New York", "Queens", "Bronx")
-label = c("Total", "Total!!White alone", "Total!!Black or African American alone",
-          "Total!!American Indian and Alaska Native alone", "Total!!Asian alone",
-          "Total!!Native Hawaiian and Other Pacific Islander alone",
-          "Total!!Some Other Race alone", "Total!!Two or More Races")
-
-key <- data.frame(vars, label) 
+label = c("Total", "White alone", "Black or African American alone",
+          "American Indian and Alaska Native alone", "Asian alone",
+          "Native Hawaiian and Other Pacific Islander alone",
+          "Some Other Race alone", "Two or More Races")
 
 census <- get_decennial(geography = "block", variables = vars, 
                         state = "NY", county = counties, year = 2010)
 
-View(census)
+totals <- census %>% group_by(variable) %>% summarize(total = sum(value)) %>% select(total)
+
+totals_by_race <- data.frame(vars, label, totals)
