3fc122f3e8de2e3fee9dce2694c5ca8ddc339265
Adnan Hoq <ahoqabir@gmail.com>
2019-07-15 15:13:50 -0400

    stop_frisk script updated

--- raw_data/stop_and_frisk.R
+++ raw_data/stop_and_frisk.R
@@ -5,69 +5,40 @@ library(naniar)
 
 #set directory to source file location
 
-#read in the stop, question, and frisk dataset for the year 2003
+#read in the stop, question, and frisk dataset for the years 2003-2018
 
-df3 <- read.csv("sqf_2003.csv")
+df3 <- read.csv("sqf_2003.csv") 
 
-#read in the stop, question, and frisk dataset for the year 2003
+df4 <- read.csv("sqf_2004.csv") 
 
-df4 <- read.csv("sqf_2004.csv")
+df5 <- read.csv("sqf_2005.csv") 
 
-#read in the stop, question, and frisk dataset for the year 2003
+df6 <- read.csv("sqf_2006.csv") %>% mutate(forceuse = NA, linecm = NA)
 
-df5 <- read.csv("sqf_2005.csv")
+df7 <- read.csv("sqf_2007.csv") 
 
-#read in the stop, question, and frisk dataset for the year 2003
+df8 <- read.csv("sqf_2008.csv") 
 
-df6 <- read.csv("sqf_2006.csv")
+df9 <- read.csv("sqf_2009.csv") 
 
-#read in the stop, question, and frisk dataset for the year 2003
+df10 <- read.csv("sqf_2010.csv") 
 
-df7 <- read.csv("sqf_2007.csv")
 
-#read in the stop, question, and frisk dataset for the year 2003
-
-df8 <- read.csv("sqf_2008.csv")
-
-#read in the stop, question, and frisk dataset for the year 2003
-
-df9 <- read.csv("sqf_2009.csv")
-
-#read in the stop, question, and frisk dataset for the year 2003
-
-df10 <- read.csv("sqf_2010.csv")
-
-#read in the stop, question, and frisk dataset for the year 2003
-
-df11 <- read.csv("sqf_2011.csv")
-
-#read in the stop, question, and frisk dataset for the year 2003
+df11 <- read.csv("sqf_2011.csv") 
 
 df12 <- read.csv("sqf_2012.csv") 
 
-#read in the stop, question, and frisk dataset for the year 2003
-
 df13 <- read.csv("sqf_2013.csv") 
 
-#read in the stop, question, and frisk dataset for the year 2003
+df14 <- read.csv("sqf_2014.csv") 
 
-df14 <- read.csv("sqf_2014.csv")
+df15 <- read.csv("sqf_2015.csv") 
 
-#read in the stop, question, and frisk dataset for the year 2003
+df16 <- read.csv("sqf_2016.csv") 
 
-df15 <- read.csv("sqf-2015.csv")
+df17 <- readxl::read_xlsx ("sqf_2017.xlsx")
 
-#read in the stop, question, and frisk dataset for the year 2003
-
-df16 <- read.csv("sqf-2016.csv")
-
-#read in the stop, question, and frisk dataset for the year 2003
-
-df17 <- readxl::read_xlsx ("sqf-2017.xlsx")
-
-#read in the stop, question, and frisk dataset for the year 2003
-
-df18<- readxl::read_xlsx ("sqf-2018.xlsx")
+df18<- readxl::read_xlsx ("sqf_2018.xlsx")
 
 
 #set the column names for 2013-18 to lowercase
@@ -81,17 +52,14 @@ colnames(df18) <- tolower(colnames(df18))
 
 #change column names from the 2006 dataset to match the naming conventions in the rest of the data
 
+df6 <- df6 %>%
+  rename("stname" = "strname", "stinter" = "strintr", "rescode" = "rescod", "premtype" = "premtyp",
+         "premname" = "prenam", "dettypcm" = "dettyp_c", "addrnum" = "adrnum", "addrpct" = "adrpct",
+         "detailcm" = "details_")
 
-colnames(df6)[colnames(df6)=="strname"] <- "stname"
-colnames(df6)[colnames(df6)=="strintr"] <- "stinter"
-colnames(df6)[colnames(df6)=="rescod"] <- "rescode"
-colnames(df6)[colnames(df6)=="premtyp"] <- "premtype"
-colnames(df6)[colnames(df6)=="prenam"] <- "premname"
-colnames(df6)[colnames(df6)=="dettyp_c"] <- "dettypcm"
-colnames(df6)[colnames(df6)=="adrnum"] <- "addrnum"
-colnames(df6)[colnames(df6)=="adrpct"] <- "addrpct"
-colnames(df6)[colnames(df6)=="details_"] <- "detailcm"
-colnames(df16)[colnames(df16)=="ï..year"] <- "year"
+
+df16 <- df16 %>%
+  rename("year" = "ï..year")
 
 
 #drop the detail1_ column in the 2006 data
@@ -100,11 +68,28 @@ df6$detail1_ <- NULL
 
 #change the "stop frisk time" column name to "stop_frisk_time" in order to match the 2017 data
 
-colnames(df18)[colnames(df18)=="stop frisk time"] <- "stop_frisk_time"
+df18 <- df18 %>%
+  rename("stop_frisk_time" = "stop frisk time")
 
 
 
 
-#bind the rows of all the data from 2003 - 2013
-sf_data1 <- bind_rows(df3,df4,df5,df6,df7,df8,df9,df10,df11,df12,df13)
+
+#bind the rows of all the data from 2003 - 2010
+data_03_10 <- rbind(df3, df4, df5, df7, df8, df9, df10) %>%
+  mutate(forceuse = NA, wepfound = NA)
+
+#bind the rows of all the data from 2011 - 2013
+data_11_13 <- rbind(df11, df12, df13) %>%
+  mutate(wepfound = NA)
+
+#bind the rows of all the data from 2003 - 2013 except 2006
+data_03_13 <- rbind(data_03_10, data_11_13)
+
+#bind the rows of all the data from 2003 - 2013 including 2006
+sf_data1 <- rbind(df3,df4,df5,df6,df7,df8,df9,df10,df11,df12,df13)
+
+sf_data2 <- rbind(sf_data1, df14, df15, df16)
+sf_2017_2018 <- rbind(df17, df18)
+
 
