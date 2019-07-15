68cfefeaf8fb8a01b2473297af8ed76851a33c07
Harpreet Gaur <harpreet.gaur97@gmail.com>
2019-07-15 17:44:25 -0400

    New file with police precinct spatial map

---
+++ raw_data/precinct_maps.R
@@ -0,0 +1,70 @@
+library(tidyverse)
+library(dplyr)
+library(tidycensus)
+library(totalcensus)
+library(sf) 
+library(tmap)
+library(tmaptools)
+library(tigris)
+library(leaflet)
+library(sp)
+library(ggplot2)
+library(ggmap)
+library(broom)
+library(httr)
+library(rgdal)
+library(rgeos)
+library(maptools)
+
+
+#Getting and reading police precincts JSON file
+r <- GET('http://services5.arcgis.com/GfwWNkhOj9bNBqoJ/arcgis/rest/services/nypp/FeatureServer/0/query?where=1=1&outFields=*&outSR=4326&f=geojson')
+police_precincts <- readOGR(content(r,'text'), 'OGRGeoJSON', verbose = F)
+
+glimpse(police_precincts)
+
+#tidying the police precincts
+police_precincts <- tidy(police_precincts)
+
+
+#Creating points from lat and long from police_precincts
+
+lats<- police_precincts$lat
+lngs<-police_precincts$long
+points <- data.frame(lats, lngs)
+
+#Creating spatial joins with inbuilt functions (make sure to load libraries on top)
+points_spdf <- points
+coordinates(points_spdf) <- ~lngs + lats 
+proj4string(points_spdf) <- proj4string(police_precincts)
+police_precincts$Precinct <- as.factor(police_precincts$Precinct)
+matches <- over(points_spdf, police_precincts)
+points <- cbind(points, matches)
+points
+
+
+#There are NA Values in precinct, this here will remove them and also count all precinct points
+points_by_precinct<- points %>% filter(!is.na(Precinct))%>% 
+  group_by(Precinct) %>%
+  summarize(num_points=n()) 
+
+
+#Tried to plot the precinct data however this needs follow up (Google API)
+plot_data <- tidy(police_precincts, region="Precinct") %>%
+  left_join(., points_by_precinct, by=c("id"="Precinct")) %>%
+  filter(!is.na(num_points))
+
+
+
+#Using leaflect to plot the precinct area polygons
+leaflet(police_precincts) %>%
+  addTiles() %>% 
+  addPolygons(popup = ~Precinct) %>%
+  addProviderTiles("CartoDB.Positron")
+
+
+#Creating spatial ggplot of the police_precinct data
+ggplot() + 
+  geom_polygon(data=police_precincts, aes(x=long, y=lat, group=group))
+
+
