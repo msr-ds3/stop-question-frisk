885a4591902bf61d9e1047cba36eaeafb77a7867
Harpreet Gaur <harpreet.gaur97@gmail.com>
2019-07-24 11:49:37 -0400

    Changes made to force intensity for black civilians

--- raw_data/force_intensity_black.R
+++ raw_data/force_intensity_black.R
@@ -83,49 +83,40 @@ black_proportions <- precinct_race %>%
   select(precinct, prop) %>%
   ungroup()
 
-View(black_proportions)
 
-#intensities <- sf_data1 %>% gather(sf_data1, `pf_hands``, `pf_hcuff``, `pf_wall", "pf_grnd", "pf_drwep", "pf_ptwep", "pf_pepsp", "pf_baton", key = "type_f_used", value = "force_used")  
 
 low_intensity <-sf_data1 %>%
-  mutate(pf_low = paste(pf_hands, pf_wall,pf_hcuff, sep = ""),
-         pf_low = if_else(grepl("Y", pf_low), 1, 0))
+                mutate(pf_low = paste(pf_hands, pf_wall,pf_hcuff, sep = ""),
+                pf_low = if_else(grepl("Y", pf_low), 1, 0))
 
-View(low_intensity)
 
-high_intensity <- sf_data1 %>% mutate(pf_high = paste(pf_grnd, pf_drwep, pf_ptwep,
-                                                      pf_baton, pf_pepsp, sep = ""),
-                                      pf_high = if_else(grepl("Y",pf_high), 1, 0))
+
+high_intensity <- sf_data1 %>% 
+                  mutate(pf_high = paste(pf_grnd, pf_drwep, pf_ptwep,
+                  pf_baton, pf_pepsp, sep = ""),
+                  pf_high = if_else(grepl("Y",pf_high), 1, 0))
+
 
 low_props_black <- low_intensity %>% filter(pf_low == 1) %>% 
-                        mutate(tally = 1) %>%
-                       group_by(addrpct, race) %>% 
-                        summarize(total = sum(tally)) %>%
-  ungroup() %>% group_by(addrpct) %>% mutate(prop = total/sum(total)) %>%
-  filter(race == "B" | race == "P") %>%
-  summarize(low_force_prop_black = sum(prop)) %>% View()
-  View()
-                       summarize(props_low_force = mean(pf_low))
-
-View(low_props_black)
-
-high_intensity_props <- high_intensity %>% group_by(addrpct) %>% summarize(props_high_force = mean(pf_high))
+                   mutate(tally = 1) %>%
+                   group_by(addrpct, race) %>% 
+                   summarize(total = sum(tally)) %>%
+                   ungroup() %>% group_by(addrpct) %>%
+                   mutate(prop = total/sum(total)) %>%
+                   filter(race == "B" | race == "P") %>%
+                   summarize(low_force_prop_black = sum(prop)) 
 
 
 
+high_props_black <- high_intensity %>% filter(pf_high == 1) %>% 
+                    mutate(tally = 1) %>%
+                    group_by(addrpct, race) %>% 
+                    summarize(total = sum(tally)) %>%
+                    ungroup() %>% group_by(addrpct) %>% 
+                    mutate(prop = total/sum(total)) %>%
+                    filter(race == "B" | race == "P") %>%
+                    summarize(high_force_prop_black = sum(prop))
 
-# find the proportion of each precinct 
-# (filter out N/A's - blocks with no corresponding precint - 
-# this is justified because no people live in these blocks (population 0))
-total_sqf_pop <- sf_data1 %>%
-  group_by(pct) %>% summarize(total_pop = sum(pct)) 
-
-total_proportion <- left_join(total_pop, total_sqf_pop, by = c("precinct"="pct")) 
-
-total_props <- total_proportion %>% 
-  mutate(props = (total_p/total_pop)) %>% na.omit(total_props$precinct)
-
-View(low_intensity_props)
 
 
 # read in police precinct shape data
@@ -133,50 +124,50 @@ r <- GET('http://services5.arcgis.com/GfwWNkhOj9bNBqoJ/arcgis/rest/services/nypp
 police_precincts <- readOGR(content(r,'text'), 'OGRGeoJSON', verbose = F)
 
 # Join the precinct shape data with the data about the precincts
-joint_prop_low <- geo_join(police_precincts, low_intensity_props, "Precinct", "addrpct")
+joint_prop_low_black <- geo_join(police_precincts, low_props_black, "Precinct", "addrpct")
 
-joint_prop_high <- geo_join(police_precincts, high_intensity_props, "Precinct", "addrpct")
+joint_prop_high_black <- geo_join(police_precincts, high_props_black, "Precinct", "addrpct")
 
 
-#Low intensity proportions for total population
-mypopup <- paste0("Precinct: ", joint_prop_low$addrpct, "<br>", 
-                  "Total Low Intensity Prop: ", joint_prop_low$props_low_force)
+#Low intensity proportions for black
+mypopup <- paste0("Precinct: ", joint_prop_low_black$addrpct, "<br>", 
+                  "Black Low Intensity Prop: ", joint_prop_low_black$low_force_prop_black)
 
 mypal <- colorNumeric(
   palette = "YlOrRd",
-  domain = joint_prop_low$props_low_force
+  domain = joint_prop_low_black$low_force_prop_black
 )
 
-leaflet(joint_prop_low) %>%
+leaflet(joint_prop_low_black) %>%
   addTiles() %>% 
-  addPolygons(fillColor = ~mypal(joint_prop_low$props_low_force),
+  addPolygons(fillColor = ~mypal(joint_prop_low_black$low_force_prop_black),
               fillOpacity = 0.7,
               weight = 1,
               popup = mypopup) %>%
   addProviderTiles("CartoDB.Positron") %>%
   addLegend(pal = mypal, 
-            values = joint_prop_low$props_low_force, 
+            values = joint_prop_low_black$low_force_prop_black, 
             position = "topleft", 
             title = "Low Intensity Prop")
 
 
 #High Intensity Proportions for total population
-mypopup2 <- paste0("Precinct: ", joint_prop_high$addrpct, "<br>", 
-                   "Total High Intensity Prop: ", joint_prop_high$props_high_force)
+mypopup2 <- paste0("Precinct: ", joint_prop_high_black$addrpct, "<br>", 
+                   "Black High Intensity Prop: ", joint_prop_high_black$high_force_prop_black)
 
 mypal2 <- colorNumeric(
   palette = "YlOrRd",
-  domain = joint_prop_high$props_high_force
+  domain = joint_prop_high_black$high_force_prop_black
 )
 
 leaflet(joint_prop_high) %>%
   addTiles() %>% 
-  addPolygons(fillColor = ~mypal2(joint_prop_high$props_high_force),
+  addPolygons(fillColor = ~mypal2(joint_prop_high_black$high_force_prop_black),
               fillOpacity = 0.7,
               weight = 1,
               popup = mypopup2) %>%
   addProviderTiles("CartoDB.Positron") %>%
   addLegend(pal = mypal2, 
-            values = joint_prop_high$props_high_force, 
+            values = joint_prop_high_black$high_force_prop_black, 
             position = "topleft", 
             title = "High Intensity Prop")
