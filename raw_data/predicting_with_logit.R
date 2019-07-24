47aeb8d12106d738c78430d3e799e49128da7a57
Etta Rapp <ettirapp@gmail.com>
2019-07-24 13:11:54 -0400

    compared probabilities of being subject to police force for different races in every precinct

--- raw_data/predicting_with_logit.R
+++ raw_data/predicting_with_logit.R
@@ -2,12 +2,18 @@ library(tidyverse)
 library(httr)
 library(rgdal)
 
+# load the logistic model that uses year and precinct as controls
 load("log_model.RData")
+# load the stop and frisk data
 load("sqf_03_13.RData")
 
+# get the list of police precincts
 r <- GET('http://services5.arcgis.com/GfwWNkhOj9bNBqoJ/arcgis/rest/services/nypp/FeatureServer/0/query?where=1=1&outFields=*&outSR=4326&f=geojson')
 police_precincts <- readOGR(content(r,'text'), 'OGRGeoJSON', verbose = F)
 
+# create a table of test data with one row for each precinct race combination
+# and using the most common outcome for every other field (column) - 
+# see the appendix for the summary statistics used for determining majorities
 race <- rep(c("White", "Black", "Hispanic", "Asian", "Other"), 77)
 sex <- as.factor(rep(1, 385))
 age <- rep(median(sf_data1$age, na.rm = TRUE), 385)
@@ -38,5 +44,14 @@ test_data <- data.frame(race, sex, age, inout,
                           cs_furtv, cs_vcrim, cs_objcs, cs_other, wepnfnd, addrpct, 
                           year)
 
+# add predictions to this test data using the logistic model
 predictions <- predict(mylogit_prec, test_data, type = "response")
 test_predictions <- data.frame(test_data, predictions)
+
+# calculate how the predicted probabilities of being subject to police force
+# compare between different races in each precinct
+discrimination <- test_predictions %>%
+  spread(race, predictions) %>%
+  mutate(HgreaterW = Hispanic > White,
+         BgreaterW = Black > White,
+         AgreaterW = Asian > White)
