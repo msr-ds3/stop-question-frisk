59bee817ae05f3f3999a6b29f3195f38a2dc7956
Harpreet Gaur <harpreet.gaur97@gmail.com>
2019-07-29 14:45:48 -0400

    Edits on white proportions by force level, created new RData file for loading precinct shape data, created shiny app data RData file

---
+++ raw_data/load_precinct_shape_file.R
@@ -0,0 +1,6 @@
+# get the list of police precincts
+r <- GET('http://services5.arcgis.com/GfwWNkhOj9bNBqoJ/arcgis/rest/services/nypp/FeatureServer/0/query?where=1=1&outFields=*&outSR=4326&f=geojson')
+police_precincts <- readOGR(content(r,'text'), 'OGRGeoJSON', verbose = F)
+
+
+save(police_precincts, file = "precinct_shape_file.RData")
