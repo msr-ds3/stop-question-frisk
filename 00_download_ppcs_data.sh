be95510186b9d181bc1e6b4b03ddbb15690d5ab8
Etta Rapp <ettirapp@gmail.com>
2020-01-17 02:15:49 -0500

    changed ppcs download file

--- 00_download_ppcs_data.sh
+++ 00_download_ppcs_data.sh
@@ -1,1 +1,11 @@
-wget dropbox.com/sh/ttnstlh9iamwqm3/AABkAdch3sw_DeXIICexXEvLa?dl=1
+wget https://www.dropbox.com/sh/ttnstlh9iamwqm3/AADPAqudb2BJnogQtoI7XWJpa/03151-0001-Data.txt?dl=1
+wget https://www.dropbox.com/sh/ttnstlh9iamwqm3/AABLve63xfU-yAv2WPo74M3ua/03151-0001-Setup.sas?dl=1
+wget https://www.dropbox.com/sh/ttnstlh9iamwqm3/AAAlsoyXYLDW_DSoIAXErXpTa/04273-0001-Data.txt?dl=1
+wget https://www.dropbox.com/sh/ttnstlh9iamwqm3/AADooBNfHlVRUbdJ54STfCwfa/04273-0001-Setup.sas?dl=1
+wget https://www.dropbox.com/sh/ttnstlh9iamwqm3/AACS_qWyC35JgZkJsJOwNQl9a/06999-0001-Data.txt?dl=1
+wget https://www.dropbox.com/sh/ttnstlh9iamwqm3/AADiQkuZNRSUEwQBlv4Nf6W6a/06999-0001-Setup.sas?dl=1
+wget https://www.dropbox.com/sh/ttnstlh9iamwqm3/AAB61kqOzXVcNu76Jxufjj6ea/20020-0001-Data.sav?dl=1
+wget https://www.dropbox.com/sh/ttnstlh9iamwqm3/AABlWYUGVvuH1YwbvNoTWmjUa/32022-0001-Data.tsv?dl=1
+wget https://www.dropbox.com/sh/ttnstlh9iamwqm3/AABJXDP9tMnGw6u0ByGl1DHKa/34276-0001-Data.rda?dl=1
+wget https://www.dropbox.com/sh/ttnstlh9iamwqm3/AABTKbB0VcPAMMRFzR5jP0oCa/36653-0001-Data.rda?dl=1
+wget https://www.dropbox.com/sh/ttnstlh9iamwqm3/AADBvgwQUBYPrUIJDcsy9Sv9a/ppcs_2015.RData?dl=1
