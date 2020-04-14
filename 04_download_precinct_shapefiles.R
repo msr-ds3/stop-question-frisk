8cbee6451f65e7791bb53d658cb8ab4399e52761
Brenda Fried <friedbrenda@gmail.com>
2020-04-14 10:03:27 -0400

    update precinct api endpoint

--- 04_download_precinct_shapefiles.R
+++ 04_download_precinct_shapefiles.R
@@ -2,7 +2,7 @@ if (!require("pacman")) install.packages("pacman")
 pacman::p_load(httr, rgdal, here)
 
 # get the police precinct shapefiles
-r <- GET('http://services5.arcgis.com/GfwWNkhOj9bNBqoJ/arcgis/rest/services/nypp/FeatureServer/0/query?where=1=1&outFields=*&outSR=4326&f=geojson')
+r <- GET('https://services5.arcgis.com/GfwWNkhOj9bNBqoJ/arcgis/rest/services/NYC_POLICE_PRECINCTS/FeatureServer/0/query?where=1=1&outFields=*&outSR=4326&f=geojson')
 police_precincts <- readOGR(content(r,'text'), 'OGRGeoJSON', verbose = F)
 
 # save shapefiles
