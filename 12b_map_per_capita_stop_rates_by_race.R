30aef576973371bfad21e4e91f7a6a63ec06e446
Etta Rapp <ettirapp@gmail.com>
2020-04-16 23:33:41 -0400

    debugging 12b

--- 12b_map_per_capita_stop_rates_by_race.R
+++ 12b_map_per_capita_stop_rates_by_race.R
@@ -8,3 +8,5 @@ webshot(url = here("figures", "white_stop_rates_by_precinct.html"),
 webshot(url = here("figures", "black_stop_rates_by_precinct.html"),
         file = here("figures", "black_stop_rates_by_precinct.png"),
         cliprect = "viewport")
+
+sessionInfo()
