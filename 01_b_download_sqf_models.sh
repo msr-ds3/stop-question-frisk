afcf92787b0eeb34fe963e2f8cbe5a89d3cf648a
Brenda Fried <friedbrenda@gmail.com>
2020-04-30 17:37:34 -0400

    script to download sqf model data

---
+++ 01_b_download_sqf_models.sh
@@ -0,0 +1,2 @@
+[ -f raw_data/model.rda] || wget -O raw_data/model.rda https://www.dropbox.com/s/a48q1zrbcbbrr08/model.rda?dl=1
+[ -f raw_data/model-no-race.rda] || wget -O raw_data/model-no-race.rda https://www.dropbox.com/s/hfghe2w464g58nr/model-no-race.rda?dl=1
